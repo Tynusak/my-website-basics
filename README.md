f1eaeb29542e126ac1c5ce4735211ff316602ff4
Kristýna Švandová <kristyna.svandova@gmail.com>
2022-08-04 19:46:50 +0200

    Theme switcher on

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # my-website-basics
 
 HTML&CSS materials for the project "My website".
+
+References
+[1] Fontawesome - icons
+[2] https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
+[3] https://codepen.io/AllThingsSmitty/pen/MmxxOz/
--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+//Dark mode switcher
+
+const toggleSwitch = document.querySelector(
+  '.theme__switcher input[type="checkbox"]',
+);
+const currentTheme = localStorage.getItem('theme')
+  ? localStorage.getItem('theme')
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
+
+const switchTheme = (e) => {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+};
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+//Hamburger menu open/close (mobile + small tablet)
 document.querySelector('#nav__button').addEventListener('click', () => {
   document.querySelector('nav').classList.toggle('nav__closed');
   document.querySelector('.fa-solid').classList.toggle('fa-bars');
